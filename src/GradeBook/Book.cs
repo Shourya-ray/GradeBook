5e1db9cef160f512217da30e64668212c02dbcf5
Shourya <Shourya.Sharma1996@gmail.com>
2021-04-24 10:43:08 -0700

    first commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace GradeBook
+	{ 
+			public class Book
+			{
+				public Book()
+				{
+					grade = new List<double>();
+				}
+				List<double> grade;
+							public void AddGrades(double Grades)
+						{
+						grade.Add(Grades);
+						}
+
+				
+			}
+	}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book Br= new Book();
+            Br.AddGrades(12.6);
+
+           //List<double> Grades =new <double>();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
